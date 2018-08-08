30e14673f27eb588d7856f60954afad63a531c0b
Clemens Fehr <clemens.fehr@sap.com>
2018-08-08 09:10:53 +0200

    working persistence eclipselink java SE example

--- secrets/src/test/java/de/fehrprice/secrets/AppTest.java
+++ secrets/src/test/java/de/fehrprice/secrets/AppTest.java
@@ -2,7 +2,13 @@ package de.fehrprice.secrets;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
 import org.junit.jupiter.api.Test;
+
+import de.fehrprice.db.EMFactory;
+import de.fehrprice.secrets.entity.User;
 
 /**
  * Unit test for simple App.
@@ -11,6 +17,18 @@ public class AppTest {
 
 	@Test
 	public void testApp() {
+		EntityManagerFactory emf = EMFactory.getEntityManager();
+		System.out.println("emf = " + emf);
+		EntityManager em = emf.createEntityManager();
+		System.out.println("em = " + em);
+		
+		em.getTransaction().begin();
+		User user = new User();
+		user.setName("clemens");
+		em.persist(user);
+		em.getTransaction().commit();
+		
+		em.close();
 		assertTrue(true);
 	}
 }
