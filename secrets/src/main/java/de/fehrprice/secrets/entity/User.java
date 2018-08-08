e756e2b2f91e2be174a9ab2b5af80378dcd3f918
Clemens Fehr <clemensfehr@gmx.de>
2018-08-08 17:45:11 +0200

    query

--- secrets/src/main/java/de/fehrprice/secrets/entity/User.java
+++ secrets/src/main/java/de/fehrprice/secrets/entity/User.java
@@ -1,25 +1,47 @@
 package de.fehrprice.secrets.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.TypedQuery;
 
 @Entity
+@NamedQuery(name = User.Query_GetAllEntities,
+            query = "select u from User u" 
+            )
 public class User {
+	public static final String Query_GetAllEntities = "GetAllEntities";
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	@Override
 	public String toString() {
 		return "User [name=" + name + "]";
+	}
+	
+	// queries:
+	public static List<User> getAllEntities(EntityManager em) {
+		TypedQuery<User> all = em.createNamedQuery(Query_GetAllEntities, User.class);
+		return all.getResultList();
 	}
 	
 }
--- secrets/src/test/java/de/fehrprice/secrets/AppTest.java
+++ secrets/src/test/java/de/fehrprice/secrets/AppTest.java
@@ -1,8 +1,11 @@
 package de.fehrprice.secrets;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.persistence.EntityManager;
@@ -21,21 +24,28 @@ public class AppTest {
 	public static final String PERSISTENCE_UNIT_NAME = "hsqldb-mem-test1";
 
 	@Test
-	public void testApp() {
+	public void testJPA() {
 		Map<String, String> props = new HashMap<String, String>();
 		props.put("eclipselink.logging.level", "INFO"); // FINE, INFO, WARNING
 		//props.put("javax.persistence.jdbc.url", "jdbc:hsqldb:file:target/testdb42XXX;shutdown=true");
         EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME, props);
-		//EntityManagerFactory emf = EMFactory.getEntityManager();
-		System.out.println("emf = " + emf);
+        assertNotNull(emf);
 		EntityManager em = emf.createEntityManager();
-		System.out.println("em = " + em);
+        assertNotNull(em);
 		
+        // write entity within new transaction
 		em.getTransaction().begin();
 		User user = new User();
 		user.setName("clemens");
 		em.persist(user);
 		em.getTransaction().commit();
+
+		// now read list of users and verify:
+		List<User> users = User.getAllEntities(em);
+		assertEquals(1, users.size());
+		User testUser = users.get(0);
+		assertEquals("clemens", testUser.getName());
+		System.out.println("User has auto created id " + testUser.getId());
 		
 		em.close();
 		assertTrue(true);
