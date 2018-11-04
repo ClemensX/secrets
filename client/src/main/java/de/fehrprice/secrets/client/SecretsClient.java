465fb953c32be21de96e1d761cf2b2e88d435d54
Clemens Fehr <clemensfehr@gmx.de>
2018-11-04 10:48:49 +0100

    handle config file

--- client/src/main/java/de/fehrprice/secrets/client/SecretsClient.java
+++ client/src/main/java/de/fehrprice/secrets/client/SecretsClient.java
@@ -1,8 +1,14 @@
 package de.fehrprice.secrets.client;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Properties;
 
 import de.fehrprice.crypto.AES;
 import de.fehrprice.crypto.Conv;
@@ -14,16 +20,16 @@ public class SecretsClient {
 	public static void main(String[] args) {
 		if (isCommand("keygen", args)) {
 			keygen(args);
-			System.exit(0);
+			done();
 		}
 		if (isCommand("server", args)) {
 			server(args);
-			System.exit(0);
+			done();
 		}
 		// check to see if setup is ok
 		if (isCommand("setup", args) || ( args.length == 0 && getSetup() == null)) {
 			setup();
-			System.exit(0);
+			done();
 		}
 		// if we reach here no command has been found
 		usage();
@@ -33,19 +39,68 @@ public class SecretsClient {
 		// setup secrets client
 		if (getSetup() == null) {
 			// no config file yet
-			System.out.println("Create config file " + getConfigFilePath() + " ?");
+			System.out.println("Create config file " + getConfigFilePath() + " ? (yes/[no])");
+			String l = readLine();
+			if (!"yes".equalsIgnoreCase(l) && !"y".equalsIgnoreCase(l)) {
+				return;
+			}
+			Properties p = new Properties();
+			p = fillDefaultProperties(p);
+			try {
+				BufferedWriter writer = Files.newBufferedWriter(getConfigFilePath(),StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+				p.store(writer, null);
+			} catch (IOException e) {
+				fail("cannot write to file " + getConfigFilePath());
+			}
+			System.out.println("created file " + getConfigFilePath());
 		}
 	}
 
-	static Path getConfigFilePath() {
+	private static final String VERSION = "version";
+	private static final String SERVER_URL = "server_url";
+	private static final String SERVER_PUBLIC_KEY = "server_public_key";
+	
+	private static Properties fillDefaultProperties(Properties p) {
+		p.clear();
+		p.put(VERSION, "1.0");
+		return p;
+	}
+
+	private static BufferedReader in = null;
+	private static String readLine() {
+		if (in == null) {
+			in = new BufferedReader(new InputStreamReader(System.in));
+		}
+		String line = null;
+		try {
+			line = in.readLine();
+		} catch (IOException e) {
+			fail("Error reading input. Abort.");
+		}
+		if (line == null) {
+			fail("Error reading input. Abort.");
+		}
+		line = line.trim();
+		return line;
+	}
+
+	private static Path getConfigFilePath() {
 		return Paths.get(System.getProperty("user.home"), ".secrets_profile");
 	}
-	private static Object getSetup() {
+	
+	private static Properties getSetup() {
 		// read config file
 		if (!Files.exists(getConfigFilePath())) {
 			return null;
 		}
-		return null;
+		try (BufferedReader reader = Files.newBufferedReader(getConfigFilePath())) {
+			Properties p = new Properties();
+			p.load(reader);
+			return p;
+		} catch (IOException e) {
+			fail("cannot read file " + getConfigFilePath());
+		}
+		return null; // cannot happen
 	}
 
 	private static void server(String[] args) {
@@ -63,7 +118,7 @@ public class SecretsClient {
 			try {
 				n = Integer.parseInt(numString);
 			} catch (Throwable t ) {
-				error("invalid number specified.");
+				usageError("invalid number specified.");
 			}
 		}
 		System.out.println("Private keys for Secrets! Never transmit, publish or store in an unsecure place:");
@@ -73,9 +128,30 @@ public class SecretsClient {
 		}
 	}
 		
-	private static void error(String msg) {
+	/**
+	 * Exit with message and usage text
+	 * @param msg
+	 */
+	private static void usageError(String msg) {
 		System.out.println(msg);
 		usage();
+		System.exit(1);
+	}
+		
+	/**
+	 * Exit with message due to internal error.
+	 * @param msg
+	 */
+	private static void fail(String msg) {
+		System.out.println(msg);
+		System.exit(1);
+	}
+		
+	/**
+	 * Exit ok after handling a command.
+	 * @param msg
+	 */
+	private static void done() {
 		System.exit(0);
 	}
 		
@@ -105,7 +181,7 @@ public class SecretsClient {
 				" ",
 				" server                interactively add or change the url of the Secrets! server", 
 				" ",
-				" setup                 interactively add or change the url of the Secrets! server", 
+				" setup                 interactively setup your Secrets! client", 
 				" "
 				);
 		System.out.println(usageString);
